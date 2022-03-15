d9b559aeee95e53b18fe7cff88740b27d908cffb
shivam <shivamnegi479@gmail.com>
2022-03-15 22:55:03 +0530

    my first commit

---
+++ tut1[print].py
@@ -0,0 +1,17 @@
+print('hello this is first tutorial')
+
+# comment 
+
+# multiline comment 
+
+"""
+this is multiline comment
+"""
+
+# python as a calulator
+print(4+9)
+
+# escape sequence 
+
+print("c:\"my name")
+
---
+++ tut2[variable].py
@@ -0,0 +1,8 @@
+var1='shivam'
+var2=22
+var3=12.4
+print(var1,var2,var3)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var3+var2)
