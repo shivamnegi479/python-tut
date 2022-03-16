294f56260bfd1ec7095b084d6e2be34978cd2271
shivam <shivamnegi479@gmail.com>
2022-03-16 20:48:29 +0530

    16 march commit first

---
+++ tut3[string_method].py
@@ -0,0 +1,39 @@
+str="hello my name is shivam"
+# print(str[0:6])
+# o/p hello 
+
+# skip the 1 character
+# print(str[0:5:2])
+# op hlo 
+
+# skip 2 charcter 
+# print(str[0:8:3])
+# op hlm
+
+# print(str[0:])
+# op hello my name is shivam
+
+# print(str[:5])
+# op hello
+
+# print(str[0:12:1])
+# op without escaping the caharacter 
+
+# print(str[3::2])
+# to uppercase 
+# print(str.upper())
+
+# to lower 
+# str2="HELLO THIS IS SHIVAM"
+# print(str2.lower())
+
+# length of string 
+# print(len(str))
+
+# reverse the string 
+# print(str[::-1])
+
+# print(str[-4:-2])
+
+a="9".join(str)
+print(a)
