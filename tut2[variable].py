1c56c7e00821594b90ec701a5e81f547c06c6382
shivam <shivamnegi479@gmail.com>
2022-03-16 10:18:11 +0530

    16 march commit

--- tut2[variable].py
+++ tut2[variable].py
@@ -1,8 +1,22 @@
-var1='shivam'
-var2=22
-var3=12.4
-print(var1,var2,var3)
-print(type(var1))
-print(type(var2))
-print(type(var3))
-print(var3+var2)
+# var1='shivam'
+# var2=22
+# var3=12.4
+# print(var1,var2,var3)
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(var3+var2)
+
+
+# a='shiva'
+# b=str(10)
+# print(100*(a+b))
+
+# ip=int(input('enter the number'))
+# print(ip)
+
+# sum of two number simple 
+
+a=input('Enter the First Number')
+b=input('Enter the Second Number')
+print("sum of a first and second number is", int(a)+int(b))
