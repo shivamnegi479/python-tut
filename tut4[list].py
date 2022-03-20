d2e9cad380f9b169fec82eae544782abfe216737
shivam <shivamnegi479@gmail.com>
2022-03-20 21:53:43 +0530

    20 mrach first commit

--- tut4[list].py
+++ tut4[list].py
@@ -13,9 +13,29 @@
 # for j in list2:
 #     print(j)
 
+
+# list=[2,3,4,5,6]
+# list2=[]
+# for i in list:
+#     list2.append(i*i)
+# print(list2)
+
+# zip function
+# l=["m","na"]
+# l1=["y","me"]
+# z=zip(l,l1)
+# for i in z:
+#     print(i)
+
+# insert elemnt in list 
 list=[2,3,4,5,6]
-list2=[]
-for i in list:
-    
-    list2.append(i*i)
-print(list2)
+list.insert(1,[21,232,[22,33],45,[45,54,[1,2]]])
+print(list[1][4][2][1])
+
+
+# lsit can be change it is mutable 
+
+li=[1,2,3,4,5,6,7]
+print(li)
+li[1]="shivam"
+print(li)
---
+++ tut5[tuple].py
@@ -0,0 +1,18 @@
+# tupple is used to strored the multiple value in a single variable it cant be change 
+from itertools import count
+
+
+tp=(1,2,4,5,5)
+print(tp)
+
+# tp[1]=23;
+to=tp.index(2)
+print(to)
+
+
+# index method and count method in tuple 
+# index - find the number of index 
+
+# count - find the number of occurence of elemnt 
+
+print(tp.count(9))
